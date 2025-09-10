64bbec6612cbe1f0f149277263b458794af3ff14
neelmyna <100953415+neelmyna@users.noreply.github.com>
2025-09-10 14:12:31 +0530

    Delete day2-springboot/game directory

--- day2-springboot/game/src/main/java/com/mtd/game/entity/Transaction.java
+++
@@ -1,5 +0,0 @@
-package com.mtd.game.entity;
-
-public class Transaction {
-
-}
--- day2-springboot/game/src/main/java/com/mtd/game/repos/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mtd.game.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mtd.game.entity.Game;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
